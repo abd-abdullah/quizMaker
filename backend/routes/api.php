88a296b79a154390a23576c8fc719289b9e7a671
Md. Abdullah <abdullah@systechdigital.com>
2021-02-24 00:16:20 +0600

    api setup done

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,17 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::namespace('App\Http\Controllers')->group(function (){
+    Route::post('login', 'Auth\AuthController@login');
+    Route::post('register', 'Auth\AuthController@register');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::middleware('auth:api')->group(function() {
+        Route::get('/user', function (){
+            return auth()->user();
+        });
+    });
 });
